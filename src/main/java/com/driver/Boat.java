f2913711a6bfbfc96f095b445b9ae9431dd8f76f
VamshiNakirikanri <vamshikrishna.nakirikanti@gmail.com>
2023-08-12 17:43:54 +0530

    adding boat

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,27 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+
+    private String name;
+    private int capacity;
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
